db039b893ecb4bd9d7eef3a64066330c989e1612
Javier Llorente <javier@opensuse.org>
2021-01-05 00:47:55 +0100

    Add script for launching WLFX

---
+++ wlfx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Script for launching WLFX
+#
+BASE_DIR=target
+APP_VERSION=1.0-SNAPSHOT
+APP_JAR=$BASE_DIR/wlfx-$APP_VERSION.jar
+JAVAFX_HOME=/usr/share/openjfx/lib
+LIB_DIR=$BASE_DIR/lib/
+
+MODULE_PATH=$JAVAFX_HOME:$LIB_DIR
+MODULES=javafx.controls,javafx.fxml,org.kordamp.ikonli.javafx,com.fasterxml.jackson.databind
+
+java --module-path $MODULE_PATH --add-modules $MODULES -jar $APP_JAR
