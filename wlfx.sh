59f29852ed729360e0f1eb8707554c6c5cede355
Javier Llorente <javier@opensuse.org>
2022-01-01 17:07:00 +0100

    Update modules - Remove jackson.databind - Add jakarta.json and jgettext

--- wlfx.sh
+++ wlfx.sh
@@ -9,6 +9,6 @@ JAVAFX_HOME=/usr/share/openjfx/lib
 LIB_DIR=$BASE_DIR/lib/
 
 MODULE_PATH=$JAVAFX_HOME:$LIB_DIR
-MODULES=javafx.controls,javafx.fxml,org.kordamp.ikonli.javafx,com.fasterxml.jackson.databind
+MODULES=javafx.controls,javafx.fxml,org.kordamp.ikonli.javafx,jakarta.json,com.javierllorente.jgettext
 
 java --module-path $MODULE_PATH --add-modules $MODULES -jar $APP_JAR
