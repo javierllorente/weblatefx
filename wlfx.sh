b864e45d4b742a3701871020eeacf4d859a081e9
Javier Llorente <javier@opensuse.org>
2022-01-09 11:33:52 +0100

    Update module list

--- wlfx.sh
+++ wlfx.sh
@@ -9,6 +9,8 @@ JAVAFX_HOME=/usr/share/openjfx/lib
 LIB_DIR=$BASE_DIR/lib/
 
 MODULE_PATH=$JAVAFX_HOME:$LIB_DIR
-MODULES=javafx.controls,javafx.fxml,org.kordamp.ikonli.javafx,jakarta.json,com.javierllorente.jgettext
+MODULES=javafx.controls,javafx.fxml,org.kordamp.ikonli.javafx
+MODULES+=,com.javierllorente.jwl,com.javierllorente.jgettext
+MODULES+=,jakarta.annotation,jersey.hk2,org.jvnet.mimepull,jakarta.activation
 
 java --module-path $MODULE_PATH --add-modules $MODULES -jar $APP_JAR
