9b5285b176e990c1bbc288768614560d08ca7533
Javier Llorente <javier@opensuse.org>
2022-01-11 19:59:47 +0100

    Update requires: remove java.naming, add jakarta.ws.rs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.javierllorente.wlfx {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.naming;
     requires java.base;
     requires java.logging;
     requires java.prefs;
@@ -9,6 +8,7 @@ module com.javierllorente.wlfx {
     requires com.javierllorente.jgettext;
     requires org.kordamp.ikonli.javafx;
     requires io.github.javadiffutils;
+    requires jakarta.ws.rs;
 
     opens com.javierllorente.wlfx to javafx.fxml;
     exports com.javierllorente.wlfx;
