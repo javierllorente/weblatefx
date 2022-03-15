c01139a5a6ad32728649e4a482eb86ed5efccf93
Javier Llorente <javier@opensuse.org>
2022-03-16 00:50:13 +0100

    Minor fix: pass window directly to initOwner()

--- src/main/java/com/javierllorente/wlfx/alert/UncommittedChangesAlert.java
+++ src/main/java/com/javierllorente/wlfx/alert/UncommittedChangesAlert.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Javier Llorente <javier@opensuse.org>
+ * Copyright (C) 2021-2022 Javier Llorente <javier@opensuse.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ public class UncommittedChangesAlert extends Alert {
 
         super(AlertType.WARNING, "", ButtonType.NO, ButtonType.YES);
 
-        initOwner(window.getScene().getWindow());   
+        initOwner(window);   
         setTitle(App.getBundle().getString("uncommittedchanges.title"));
         setContentText(buildContextText(actionType));
         setupButtons();
