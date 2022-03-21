92f753c4f34fedf3ff7f075273d968bf18321912
Javier Llorente <javier@opensuse.org>
2022-03-21 20:53:40 +0100

    Lowercase variables (not exported)

--- buildruntime.sh
+++ buildruntime.sh
@@ -3,13 +3,13 @@
 # Builds a custom runtime image with the required JavaFX modules
 #
 JAVA_HOME=/usr/lib64/jvm/java-17-openjdk-17
-BUILD_DIR=./target
-JAVAFX_HOME=$BUILD_DIR/javafx
-JLINK_PATH=$JAVA_HOME/bin/
+build_dir=./target
+JAVAFX_HOME=$build_dir/javafx
+jlink_path=$JAVA_HOME/bin/
 
-MODULE_PATH=$JAVAFX_HOME
-MODULES=javafx.controls,javafx.fxml,java.base,java.logging,java.prefs,jdk.crypto.ec
+module_path=$JAVAFX_HOME
+modules=javafx.controls,javafx.fxml,java.base,java.logging,java.prefs,jdk.crypto.ec
 
 echo "Building runtime image..."
-$JLINK_PATH/jlink --verbose --strip-debug --no-header-files --no-man-pages --module-path $MODULE_PATH --add-modules $MODULES\
- --output $BUILD_DIR/runtime/
+$jlink_path/jlink --verbose --strip-debug --no-header-files --no-man-pages --module-path $module_path --add-modules $modules\
+ --output $build_dir/runtime/
