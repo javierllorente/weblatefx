c8bb2027249bb8806accd2acc6fd46cb98414d0e
Javier Llorente <javier@opensuse.org>
2022-03-21 21:19:03 +0100

    Detect app version automatically

--- wlfx.sh
+++ wlfx.sh
@@ -3,8 +3,10 @@
 # Script for launching WLFX
 #
 base_dir=target
-app_version=1.0-SNAPSHOT
-app_jar=$base_dir/dist/wlfx-$app_version.jar
+name=wlfx
+jar_filepath=`ls $base_dir/dist/$name*.jar`
+jar_filename=$(basename -- "$jar_filepath")
+version=${jar_filename:${#name}+1:-4}
 JAVAFX_HOME=$base_dir/javafx
 lib_dir=$base_dir/dist/lib
 
@@ -15,4 +17,4 @@ modules+=,jakarta.annotation,jersey.hk2,org.jvnet.mimepull,jakarta.activation
 modules+=,org.glassfish.json.jaxrs,org.glassfish.hk2.api
 
 java --module-path $module_path --add-modules $modules\
- --add-opens org.glassfish.json.jaxrs/org.glassfish.json.jaxrs=org.glassfish.hk2.utilities -jar $app_jar
+ --add-opens org.glassfish.json.jaxrs/org.glassfish.json.jaxrs=org.glassfish.hk2.utilities -jar $jar_filepath
