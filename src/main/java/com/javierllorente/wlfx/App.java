8fd20d694a6c3745a93d333496c7c81096889a3b
Javier Llorente <javier@opensuse.org>
2022-03-24 12:03:31 +0100

    Bump version to 0.9.9

--- src/main/java/com/javierllorente/wlfx/App.java
+++ src/main/java/com/javierllorente/wlfx/App.java
@@ -35,7 +35,7 @@ import javafx.scene.image.Image;
 public class App extends Application {
 
     public static final String NAME = "WLFX";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.9.9";
     public static final String USER_AGENT = NAME + "/" + VERSION;
     public static final int WINDOW_WIDTH = 1024;
     public static final int WINDOW_HEIGHT = 768;
